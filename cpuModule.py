9b351fad497ef7b3388e5752afcf8d753ac8bb87
Matheus A. Araujo <75196977+matheusaraujotrd@users.noreply.github.com>
2022-11-28 22:42:02 -0300

    +refactor
    Refactored autoplacement. Now autoplacement functions (should) work for any piece under any circunstances.

---
+++ cpuModule.py
@@ -0,0 +1,70 @@
+from random import randint
+
+def shipPlacement(grid, ship, gridSize):
+    gridSize = gridSize
+    grid = grid
+    shipPosition = []
+    if ship == "carrier":
+        shipSize = 5
+        shipTag = 4
+    elif ship == "battleship":
+        shipSize = 4
+        shipTag = 3
+    elif ship == "cruiser":
+        shipSize = 3
+        shipTag = 2
+    elif ship == "destroyer":
+        shipSize = 2
+        shipTag = 1
+
+    # direction == 0: up, 1: down, 2: left, 3: right
+    direction = randint(0, 3)
+    shipStart = (randint(0, gridSize - 1), randint(0, gridSize - 1))
+    moveDirection, isValid = validDirection(grid, direction, shipSize, shipStart, gridSize)
+    while isValid == False:
+        direction = randint(0, 3)
+        shipStart = (randint(0, gridSize - 1), randint(0, gridSize - 1))
+        moveDirection, isValid = validDirection(grid, direction, shipSize, shipStart, gridSize)
+    shipDeployment(grid, shipSize, shipTag, shipStart, moveDirection)
+
+def validDirection(grid, direction, shipSize, shipStart, gridSize):
+    shipPosition = []
+    if direction == 0:
+        if shipStart[0] - shipSize >= 0:
+            for x in range(shipSize):
+                shipPosition.append(grid[shipStart[0] - x][shipStart[1]])
+                if shipPosition.count(None) == shipSize:
+                    return [direction, True]
+    elif direction == 1:
+        if shipStart[0] + shipSize <= gridSize:
+            for x in range(shipSize):
+                shipPosition.append(grid[shipStart[0] + x][shipStart[1]])
+                if shipPosition.count(None) == shipSize:
+                    return [direction, True]
+    elif direction == 2:
+        if shipStart[1] - shipSize >= 0:
+            for x in range(shipSize):
+                shipPosition.append(grid[shipStart[0]][shipStart[1] - x])
+                if shipPosition.count(None) == shipSize:
+                    return [direction, True]
+    elif direction == 3:
+        if shipStart[1] + shipSize <= gridSize:
+            for x in range(shipSize):
+                shipPosition.append(grid[shipStart[0]][shipStart[1] + x])
+                if shipPosition.count(None) == shipSize:
+                    return [direction, True]
+    return [direction, False]
+
+def shipDeployment(grid, shipSize, shipTag, shipStart, moveDirection):
+    if moveDirection == 0:
+        for i in range(shipSize):
+            grid[shipStart[0] - i][shipStart[1]] = shipTag
+    elif moveDirection == 1:
+        for i in range(shipSize):
+            grid[shipStart[0] + i][shipStart[1]] = shipTag
+    elif moveDirection == 2:
+        for i in range(shipSize):
+            grid[shipStart[0]][shipStart[1] - i] = shipTag
+    elif moveDirection == 3:
+        for i in range(shipSize):
+            grid[shipStart[0]][shipStart[1] + i] = shipTag
--- first_test.py
+++ first_test.py
@@ -1,4 +1,5 @@
-from random import randint
+import cpuModule
+
 grid_cpu = []
 grid_player = []
 grid_size = 10
@@ -9,61 +10,6 @@ def start_board(grid, board_size: int):
     for line in grid:
         for i in range(board_size):
             line.append(None)
-
-def carrier_placement(grid):
-    # carrier = 1
-    carrier_size = 5
-    carrier_placed = False
-    while not carrier_placed:
-        carrier_start = (randint(0, grid_size - 1), randint(0, grid_size - 1))
-        direction = randint(0, 3)
-        # 0: up, 1: down, 2: left, 3: right
-        if direction == 0:
-            if carrier_start[0] - carrier_size >= 0:
-                carrier_slots = [grid[carrier_start[0]][carrier_start[1]],
-                grid[carrier_start[0] - 1][carrier_start[1]],
-                grid[carrier_start[0] - 2][carrier_start[1]],
-                grid[carrier_start[0] - 3][carrier_start[1]],
-                grid[carrier_start[0] - 4][carrier_start[1]]]
-                if carrier_slots.count(None) == carrier_size:
-                    for i in range (carrier_size):
-                        grid[carrier_start[0] - i][carrier_start[1]] = 1
-                    carrier_placed = True
-        
-        elif direction == 1:
-            if carrier_start[0] + carrier_size <= grid_size:
-                carrier_slots = [grid[carrier_start[0]][carrier_start[1]],
-                grid[carrier_start[0] + 1][carrier_start[1]],
-                grid[carrier_start[0] + 2][carrier_start[1]],
-                grid[carrier_start[0] + 3][carrier_start[1]],
-                grid[carrier_start[0] + 4][carrier_start[1]]]
-                if carrier_slots.count(None) == carrier_size:
-                    for i in range (carrier_size):
-                        grid[carrier_start[0] + i][carrier_start[1]] = 1
-                    carrier_placed = True
-
-        elif direction == 2:
-            if carrier_start[1] - carrier_size >= 0:
-                carrier_slots = [grid[carrier_start[0]][carrier_start[1]],
-                grid[carrier_start[0]][carrier_start[1] - 1],
-                grid[carrier_start[0]][carrier_start[1] - 2],
-                grid[carrier_start[0]][carrier_start[1] - 3],
-                grid[carrier_start[0]][carrier_start[1] - 4]]
-                if carrier_slots.count(None) == carrier_size:
-                    for i in range (carrier_size):
-                        grid[carrier_start[0]][carrier_start[1] - i] = 1
-                    carrier_placed = True    
-        elif direction == 3:
-            if carrier_start[1] + carrier_size <= grid_size:
-                carrier_slots = [grid[carrier_start[0]][carrier_start[1]],
-                grid[carrier_start[0]][carrier_start[1] + 1],
-                grid[carrier_start[0]][carrier_start[1] + 2],
-                grid[carrier_start[0]][carrier_start[1] + 3],
-                grid[carrier_start[0]][carrier_start[1] + 4]]
-                if carrier_slots.count(None) == carrier_size:
-                    for i in range (carrier_size):
-                        grid[carrier_start[0]][carrier_start[1] + i] = 1
-                    carrier_placed = True
         
                                 
 
@@ -75,8 +21,15 @@ def print_board(grid):
             if sqr == None:
                 grid_line += "| ░ |"
             else:
-                grid_line += f"| █ |"
+                grid_line += f"| {sqr} |"
         print(grid_line, "\n")
 
 start_board(grid_player, grid_size)
+cpuModule.shipPlacement(grid_player, "carrier", grid_size)
+cpuModule.shipPlacement(grid_player, "battleship", grid_size)
+cpuModule.shipPlacement(grid_player, "cruiser", grid_size)
+cpuModule.shipPlacement(grid_player, "destroyer", grid_size)
+cpuModule.shipPlacement(grid_player, "destroyer", grid_size)
+cpuModule.shipPlacement(grid_player, "destroyer", grid_size)
+cpuModule.shipPlacement(grid_player, "battleship", grid_size)
 print_board(grid_player)
